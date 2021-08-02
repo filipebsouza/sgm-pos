1425f0181d8662e753dc9c1c1c5d333813c337e0
Filipe <filipe.bsouza@gmail.com>
2021-08-02 08:13:34 -0400

    Alterações

--- src/SGM.Apresentacao/Frontend/src/app/pages/unauthorized/unauthorized.component.ts
+++ src/SGM.Apresentacao/Frontend/src/app/pages/unauthorized/unauthorized.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Usuario } from 'src/app/models/usuario.model';
+import { JWTTokenService } from 'src/app/services/security/jwt-token.service';
 
 @Component({
     selector: 'app-unauthorized',
     templateUrl: './unauthorized.component.html'
 })
-export class UnauthorizedComponent {
-    constructor(private router: Router) { }
+export class UnauthorizedComponent implements OnInit {
+    usuario: Usuario;
+
+    constructor(private _jwtService: JWTTokenService,
+        private router: Router) { }
+
+    ngOnInit(): void {
+        this._jwtService.emissorDeUsuarioLogado.subscribe(usuario => this.usuario = usuario);
+        if (!this.usuario) {
+            this.usuario = this._jwtService.getUsuario();
+        }
+    }
 
     irParaTelaDeLogin() {
         this.router.navigate(['/login']);
